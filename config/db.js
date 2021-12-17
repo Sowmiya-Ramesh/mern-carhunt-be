db4073825eb3b2c6eb244cf83faac56e7c6b85a7
Sowmiya-Ramesh <miyaramesh9944@gmail.com>
2021-12-17 13:01:41 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+// wraping it in try and catch
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
---
+++ data/products.js
@@ -0,0 +1,63 @@
+const products = [{ _id: '1',
+name: 'Mini Cooper Convertible 2021',
+image: 'https://stimg.cardekho.com/images/carexteriorimages/930x620/Mini/Cooper-Convertible/5182/1624430518066/front-left-side-47.jpg',
+description: 'The Mini Cooper Convertible has 1 Petrol Engine on offer. The Petrol engine is 1998 cc . It is available with Automatic transmission.Depending upon the variant and fuel type the Cooper Convertible has a mileage of 16.72 kmpl . The Cooper Convertible is a 4 seater 4 cylinder car and has length of 3850mm, width of 1727mm and a wheelbase of 2495mm.',
+brand: 'BMW',
+category: 'Car',
+price: $58361 ,
+countInStock: 5,
+rating: 4.0,
+numReviews: 4 ,
+},
+{ 
+name: '2016 Maruti Suzuki Swift LXI (O)',
+image: 'https://d308ljkq6e62o1.cloudfront.net/img/FTxM6HLvSoG_UHCN0wybrA/mobile/file',
+description: '25,108 Kms Dont Waste Your Time With High Prices, Get The Right Vehicle For You For The Right Price. Worried About Buying The Wrong Car? ',
+brand: 'BMW',
+category: 'Car',
+price: 456 ,
+countInStock: 5,
+rating: 3.0,
+numReviews: 6 ,
+},
+
+
+{ 
+name: '2016 Maruti Suzuki Swift LXI (O)',
+image: 'https://d308ljkq6e62o1.cloudfront.net/img/2uuM%2BRDMSoOfN7yBhENJBg/mobile/file',
+description: '25,108 Kms Dont Waste Your Time With High Prices, Get The Right Vehicle For You For The Right Price. Worried About Buying The Wrong Car? ',
+brand: 'BMW',
+category: 'Car',
+price: 562 ,
+countInStock: 6,
+rating: 4.3,
+numReviews: 5 ,
+},
+
+
+{ 
+name: 'Maruti Suzuki Swift LXI (O)',
+image: 'https://d308ljkq6e62o1.cloudfront.net/img/TBxarBp1QTGXL2oHE0bL4w/tab/file',
+description: '25,108 Kms Dont Waste Your Time With High Prices, Get The Right Vehicle For You For The Right Price. Worried About Buying The Wrong Car? ',
+brand: 'BMW',
+category: 'Car',
+price: 456 ,
+countInStock: 5,
+rating: 4.0,
+numReviews: 4 ,
+},
+
+
+{ 
+name: ' Suzuki Swift LXI (O)',
+image: 'https://d308ljkq6e62o1.cloudfront.net/img/TBxarBp1QTGXL2oHE0bL4w/tab/file',
+description: '25,108 Kms Dont Waste Your Time With High Prices, Get The Right Vehicle For You For The Right Price. Worried About Buying The Wrong Car? ',
+brand: 'BMW',
+category: 'Car',
+price: 456 ,
+countInStock: 5,
+rating: 4.0,
+numReviews: 4 ,
+}]
+
+export default products
